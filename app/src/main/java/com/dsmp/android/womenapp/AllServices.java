4df6e01fdb37cf5ab0b18f7639dd6a900a4bdaed
Vipul Gaikwad <31430033+1RomAn7@users.noreply.github.com>
2018-02-24 22:30:21 +0530

    New Additon Activities

---
+++ app/src/main/java/com/dsmp/android/womenapp/AllServices.java
@@ -0,0 +1,13 @@
+package com.dsmp.android.womenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_services);
+    }
+}
--- app/src/main/java/com/dsmp/android/womenapp/MainActivity.java
+++ app/src/main/java/com/dsmp/android/womenapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dsmp.android.womenapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -10,9 +13,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Service service ;
+    Button buttonAllService;
+    Button buttonAdvanceSearch;
 
 
     FirebaseDatabase database;
@@ -23,12 +28,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        buttonAllService =(Button) findViewById(R.id.buttonAllService);
+        buttonAdvanceSearch=(Button)findViewById(R.id.buttonAdvanceSerch);
 
+
+        buttonAllService.setOnClickListener(this);
+
+
+
+        buttonAdvanceSearch.setOnClickListener(this);
+
+        buttonAllService.setOnClickListener(this);
+
+
+        buttonAdvanceSearch.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(buttonAllService.getId()==view.getId())
+        {
+
+            Intent intent =new Intent(this,AllServices.class);
+            startActivity(intent);
+
+        }
+
+        if(buttonAdvanceSearch.getId()==view.getId())
+        {
+
+            Intent intent =new Intent(this,AllServices.class);
+            startActivity(intent);
+
+        }
 
 
 
 
 
     }
+
+
+
 }
